5c708b9d86972733cdba98165f6737c7df0ae681
akku333 <47693955+akku333@users.noreply.github.com>
2019-08-16 22:20:01 +0100

    Update forest.R

--- forest.R
+++ forest.R
@@ -7,7 +7,7 @@ library("readr")
 
 
 
-forest_inspection <- read_csv ("C:/Users/nikhi/Downloads/PDA_proj/Forest_data/Inspection.csv")
+forest_inspection <- read_csv ("C:/Users/neerajkulshrestha/Downloads/PDA_proj/Forest_data/Data.csv")
 names(forest_inspection)
 forest_inspection1 <- forest_inspection[,-c(8,9,11,12,14,15,16,18,20,21,22,23,27,28)]
 names(forest_inspection1)
@@ -17,5 +17,5 @@ forest_inspection1$SWDamageRating <- as.numeric(forest_inspection1$SWDamageRatin
 class(forest_inspection1$SWTreeConditionRating)
 head(forest_inspection1$SWDamageRating)
 
-write.csv(forest_inspection1, "Inspection1.csv")
-write.csv(tree_points1, "Treepoint1.csv")
+write.csv(forest_inspection1, "d1.csv")
+write.csv(tree_points1, "d2.csv")
