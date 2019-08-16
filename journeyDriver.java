b046a5c53fea38b1d33ef6ef3f69a03ab78e1b32
akku333 <47693955+akku333@users.noreply.github.com>
2019-08-16 22:13:35 +0100

    Update and rename RiskDamageDriver.java to journeyDriver.java

--- journeyDriver.java
+++ journeyDriver.java
@@ -12,16 +12,16 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
 
-public class RiskDamageDriver {
+public class journeyDriver {
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		conf.set("fs.defaultFS", "hdfs://localhost:54310");
 		conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
 		Job job = Job.getInstance(conf, "Group By 3  and avg of Field 6");
 		//Job job = Job.getInstance();
-		job.setJarByClass(RiskDamageDriver.class);
-		job.setMapperClass(RiskDamageMapper.class);
-		job.setReducerClass(RiskDamageReducer.class);
+		job.setJarByClass(JourneyDriver.class);
+		job.setMapperClass(journeyMapper.class);
+		job.setReducerClass(journeyReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path("hdfs://localhost:54310/user/hduser/combined/part-m-00000"));
